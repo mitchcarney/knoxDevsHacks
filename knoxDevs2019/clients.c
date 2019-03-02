bbbd6248e6d22173ba0c647309a63898abb097a6
Tyler Christian <tylerchristian@Tylers-MacBook-Pro-2.local>
2019-03-02 17:31:37 -0500

    added clits.c

---
+++ knoxDevs2019/clients.c
@@ -0,0 +1,15 @@
+#include "clients."
+#include <iostream>
+#include <stdio.h>
+
+
+printClient(client c){
+                asctime(*timePtr);
+                printf("%s, %s\n" curPtr->client.lastName, curPtr->client.firstName);
+                printf("Appointment Time: %d / %d / %d\n", month, day, year);
+                timePtr->tm_mday, timeptr->tm_hour,
+                timeptr->tm_min, timeptr->tm_sec,
+                1900 + timeptr->tm_year;
+                return result;
+}
+
