183942c017a57665cafa39ff1b973b021d1ecbb8
Tyler Lovett <tylerlovett@Tylers-MacBook-Pro.local>
2019-03-02 21:31:47 -0500

    update

--- knoxDevs2019/main.cpp
+++ knoxDevs2019/main.cpp
@@ -5,24 +5,54 @@
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
+#include <istream>
+
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+
+#include "utilities.h"
 #include "clients.h"
 #include "list.h"
 #include "node.h"
 
-#define MAX_FILENAME 51;
+//#define MAX_FILENAME 51;
+
+
+
+
 
 int main(int argc, char* argv[]) {
 
- char filename[MAX_FILENAME];
+    
+    /*
+   //Spawn a child to run the program.
+    pid_t pid=fork();
+    if (pid==0) { // child process
+        static char *argv[]={"checkDate",NULL,NULL};
+        execv("/bin/checkDate",argv);
+        exit(127); // only if execv fails
+    }
+    else { // pid!=0; parent process
+        waitpid(pid,0,0); // wait for child to exit
+    }
+*/
+    
+    int MAX_FILENAME = 51;
+    
+    char filename[MAX_FILENAME];
  ifstream input;
- client clients;
- int option;
+ struct client_t clients;
+ string option;
  if (argc<2) {
 	printf("Error: app filename\n");
+     return -1;
  }
  
  else {
- 	strcpy(filename, argv[1]); //name of file: database.txt
+
+     strcpy(filename, argv[1]); //name of file: database.txt
  	input.open(filename);
  }
 
@@ -38,52 +68,54 @@ int main(int argc, char* argv[]) {
 	exit (1);
  }
  else {
-	putback(c); //puts character back in file
+	//----------------putback(c); //puts character back in file
  }
 
- dbl_linked_list_t list; 
+ struct list_t list;
  node_t* nPtr;
- createList(&list);
- fread (&clients,sizeof(client),1,in);
+    //0----------------createList(list);
+// -------------------fread (&clients,sizeof(client_t),1,in);
  while ( !input.eof() ){
- 	nPtr=initNode(client);
- 	insertNode(&list, nPtr);
-	fread(&clients, sizeof(client), 1, in);
+// 	----------------nPtr=initNode(client_t);
+ 	//------------------insertNode(&list, nPtr);
+	//-------------------fread(&clients, sizeof(client), 1, in);
 	//print the data here 
 }
 
  input.close();
- while (option!=4) { 	
+ while (option!="4") {
  printf("Welcome to HealthPort Services\n");
  printf("------------------------------\n");
  printf("1) Make Appointment\n");
  printf("2) Search For Appointment\n");
  printf("3) Cancel Appointment\n");
  printf("4) Exit\n");
- printf(Option>> ");
- scanf("%d\n", option);
+//------------------ printf(Option>> ");
+ scanf("%s\n", &option);
  
- for (i=0; option[i]!='\0'; i++) {
+ for (int i=0; option[i]!='\0'; i++) {
  	if (isdigit(option[i]) ) {
-		atoi(option);
+		atoi(option.c_str());
         }
         else {
                 printf("Error, must be a digit\n");
                 break;
         }
  }
- option=atoi(option);
-
- switch(option){
+     
+ int choice = atoi(option.c_str());
+     string nameKey;
+     string tempKey;
+ switch(choice){
 
 	case 1:
 		
 		break;
 	case 2: 
-		Printf("Enter a Person to Search");
-		scanf("%s", nameKey);
-		makeLowerCase(nameKey,tempKey);
-		searchByName(nameKey,list)
+		printf("Enter a Person to Search");
+		scanf("%s", &nameKey);
+		//makeLowerCase(nameKey,tempKey);
+		//searchByName(nameKey,list)
 		break;
 	case 3:
 		break;
@@ -94,4 +126,6 @@ int main(int argc, char* argv[]) {
 		printf("Error, Option must be between 1-4\n");
 
 return (0);
+}
+ }
 }
